bc7799cc73a7fe0d524f5c9f4241e2b47db2d2af
c5-Studios <37878468+c5-Studios@users.noreply.github.com>
2022-01-19 16:08:58 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,123 @@
---test
+local Suite = {}
+
+local plugin = nil
+
+local Widgets = {}
+local DockWidgetReadList = {"InitialDockState", "EnabledOnInit", "OverridePreviousState", "FloatingSizeX", "FloatingSizeY", "MinimumSizeX", "MinimumSizeY"}
+
+Suite.PrimaryWidget = nil
+Suite.PrimaryButton = nil
+
+local UIHandler = {} do
+	local Objects = {}
+	
+	local Theme = {
+		Background = Color3.fromRGB(46, 46, 46),
+
+		Bar = {
+			Background = Color3.fromRGB(0,0,0),
+			BorderColor = Color3.fromRGB(21, 21, 21),
+			BarColor = Color3.fromRGB(255,255,255)
+		},
+
+		Text = {
+			Front = Color3.fromRGB(255,255,255),
+			Back = Color3.fromRGB(0,0,0),
+		}
+	}
+	
+	function UIHandler:SetTheme(...)
+		Theme = ...
+	end
+
+	function UIHandler:GetTheme()
+		return Theme
+	end
+
+	function UIHandler:NewObject(InstanceType, Properties)
+		local Object = Instance.new(InstanceType)
+
+		Objects[Properties.Name or ""] = Object
+
+		if Properties.Parent == nil then
+			Object.Parent = Objects.Background
+		end
+
+		for Index, Property in pairs(Properties) do
+			Object[Index] = Property
+		end
+
+		return Object
+	end
+
+	function UIHandler:GetObject(...)
+		return Objects[...]
+	end
+
+	function UIHandler:Init(Window)
+		Window:ClearAllChildren()
+
+		self:NewObject("Frame", {
+			Name = "Background",
+			Size = UDim2.new(1, 0, 1, 0),
+			BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+			Parent = Window
+		})
+	end
+end
+
+function Suite:CreateWidget(WidgetInformation)
+	local CompressedDockWidgetInformation = {}
+
+	for Index = 1, #DockWidgetReadList do -- Convert the dictionary into an array
+		local DictionaryIndex = DockWidgetReadList[Index]
+		local DictionaryValue = WidgetInformation[DictionaryIndex]
+
+		if DictionaryValue == nil then
+			error("Expected value for index " .. DictionaryIndex .. ", got nil")
+		else
+			CompressedDockWidgetInformation[Index] = DictionaryValue
+		end
+	end
+
+	local DockWidgetInformation = DockWidgetPluginGuiInfo.new(unpack(CompressedDockWidgetInformation))
+	local WidgetName = WidgetInformation.Name
+	local FormattedWidgetName = WidgetName:gsub(" ", "")
+	local NewWidget = plugin:CreateDockWidgetPluginGui(WidgetName, DockWidgetInformation)
+
+	NewWidget.Name = FormattedWidgetName
+	NewWidget.Title = WidgetName
+
+	Widgets[FormattedWidgetName] = NewWidget
+
+	return NewWidget
+end
+
+function Suite:Load(Plugin)
+	local Toolbar = Plugin:CreateToolbar("iG Plugin Suite")
+	
+	plugin = Plugin
+	
+	Suite.PrimaryWidget = Suite:CreateWidget({
+		Name = "iG Suite",
+		InitialDockState = Enum.InitialDockState.Float,
+		EnabledOnInit = false,
+		OverridePreviousState = false,
+		FloatingSizeX = 500,
+		FloatingSizeY = 300,
+		MinimumSizeX = 500,
+		MinimumSizeY = 300
+	})
+	
+	Suite.PrimaryButton = Toolbar:CreateButton("Open", "Opens the suite", "rbxassetid://8567187012")
+	
+	Suite.PrimaryButton.Click:Connect(function()
+		Suite.PrimaryWidget.Enabled = not Suite.PrimaryWidget.Enabled
+	end)
+	
+	UIHandler:Init(Suite.PrimaryWidget)
+	
+	warn("iG suite loaded!")
+end
+
+return Suite
